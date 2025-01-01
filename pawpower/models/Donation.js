c4e561b095e302eab39696dcdd3a38dff906afd7
Farhan Tanvir <ft231411@gmail.com>
2025-01-01 21:19:41 +0600

    Add files via upload

--- pawpower/models/Donation.js
+++ pawpower/models/Donation.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-// Define the Donation schema
 const DonationSchema = new mongoose.Schema({
     donorName: { type: String, required: true },
     amount: { type: Number, required: true },
     message: { type: String },
+    category: { type: String, required: true },
     donatedAt: { type: Date, default: Date.now },
 });
 
